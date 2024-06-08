7b73f0008127f8a4eaed6003e86c169e7c28d34f
EGOPOS <egopos@yahoo.com>
2024-06-08 12:03:14 +0300

    empty base game manager, sound manager, and fog on main

---
+++ game/game_manager.gd
@@ -0,0 +1,13 @@
+extends Node
+
+signal enemy_turn
+signal player_turn
+
+func player_confirm_turn():
+	pass
+
+func _enemy_turn():
+	pass
+
+func _spawn_unit():
+	pass
---
+++ game/main/main.gd
@@ -0,0 +1,9 @@
+extends Node
+
+@export var background_music_stream: AudioStream
+
+@onready var sound_manager = $SoundManager
+
+func _ready():
+	sound_manager.set_looped(sound_manager.play(background_music_stream))
+	
