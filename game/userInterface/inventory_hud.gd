d1027f2929669d290ace607bebc76d97f557879e
sudo-m3zim <nizilplay2004@gmail.com>
2024-06-09 00:00:01 +0300

    Added inventory to monitor

--- game/userInterface/inventory_hud.gd
+++ game/userInterface/inventory_hud.gd
@@ -1,3 +1,18 @@
 extends Control
 
 class_name InventoryHud
+
+@export var inputs: Array[String]
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func show_invntory() -> void:
+	animation_player.play("AppearAnimation")
+
+func _unhandled_input(event: InputEvent) -> void:
+	for input in inputs:
+		if event.is_action_pressed(input):
+			check_input(input)
+
+func check_input(input: String) -> void:
+	pass
