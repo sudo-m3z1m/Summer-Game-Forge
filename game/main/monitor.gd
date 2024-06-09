ede5a769f6749472e95bf9b2c9b73ced646dffe2
sudo-m3zim <nizilplay2004@gmail.com>
2024-06-09 03:09:02 +0300

    Removed test item for inventory

--- game/main/monitor.gd
+++ game/main/monitor.gd
@@ -1,15 +1,10 @@
 class_name Monitor extends Node3D
 
-@export var test_item: Item
-
 @onready var item_description_screen: ItemDescriptionHud = %ItemDescription
 @onready var reviews_screen: Control = %Reviews
 @onready var inventory_screen: InventoryHud = %InventoryHud
 @onready var current_screen: Control = reviews_screen
 @onready var camera_marker: Marker3D = $CameraMarker
-
-func _ready() -> void:
-	display_inventory(test_item)
 
 func change_current_screen(new_screen: Control) -> void:
 	current_screen.hide()
